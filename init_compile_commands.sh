214194ab428592f693a4979078b14aba75d35402
Juyeong Maing <mjy9088@naver.com>
2024-10-11 22:05:47 +0900

    fix init_compile_commands

--- init_compile_commands.sh
+++ init_compile_commands.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-cd "$(dirname "$0")"
-
-mkdir -p .cache
-echo "int main() { return 0; }" | clang -x c - -o /dev/null -MJ .cache/tmp.json.part
-(echo "[" && cat .cache/tmp.json.part && echo "]") > compile_commands.json
+echo '[{"directory":"'"$(pwd)"'","arguments":["clang","-Iinclude","-x","c","-std=c99","-pedantic","-c","file.c","-o","file.o"],"file":"file.c" }]' > compile_commands.json
