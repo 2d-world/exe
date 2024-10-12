efa74a2c4372f0e01b0239277d084eb06fefd155
Juyeong Maing <mjy9088@naver.com>
2024-10-12 21:35:42 +0900

    apply t.h changes

--- src/t.h
+++ src/t.h
@@ -18,10 +18,18 @@ struct THandle {
   void (*free)(void *actual_handle);
   void *actual_handle;
 };
-typedef struct THandle *(*TOn)(T context, const char *event_name, void *args);
-typedef err_t (*TPlugin)(T context, TOn on);
+typedef err_t (*TPlugin)(T context, TMap_search search);
 
 typedef T (*tInit)(void);
 typedef err_t (*tRegisterPlugin)(T self, TPlugin plugin);
 typedef err_t (*tStart)(T self);
 typedef void (*tDestroy)(T self);
+
+#define KEY_BUILTIN_UTIL_T_MALLOC "builtin.util.t_malloc"
+#define KEY_BUILTIN_UTIL_T_REALLOC "builtin.util.t_realloc"
+#define KEY_BUILTIN_UTIL_T_MEMDUP "builtin.util.t_memdup"
+#define KEY_BUILTIN_UTIL_T_STRDUP "builtin.util.t_strdup"
+#define KEY_BUILTIN_TMAP_NEW "builtin.TMap.new"
+#define KEY_BUILTIN_TMAP_INSERT "builtin.TMap.insert"
+#define KEY_BUILTIN_TMAP_SEARCH "builtin.TMap.search"
+#define KEY_BUILTIN_TMAP_DELETE "builtin.TMap.delete"
